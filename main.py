162ac8dabc09ebcabc26bf049c2808f98e7ee525
Brunoab80 <brunoab80@icloud.com>
2024-09-01 18:38:25 +0000

    Definido o ano com mais jogos

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ f= open ("jogos.csv")
 
 gratuitos=0
 pagos=0
-jogos_por_ano= {  }
+jogos_por_ano= { }
+maior_ano=0
 
 for line in f:
   lista= line.strip().split(',')
@@ -21,9 +22,14 @@ for line in f:
     jogos_por_ano[ano] += 1
   else:
     jogos_por_ano[ano] = 1
+#----Determinar o ano com mais jogos novos-------------------
+
 
 
 print (gratuitos/(gratuitos+pagos)*100,'% de jogos gratuitos')
 print (pagos/(pagos+gratuitos)*100,'% de jogos pagos') 
 for ano, quantidade in jogos_por_ano.items():
+  maior_ano= max(jogos_por_ano.values())
   print(f"Ano {ano}: {quantidade} jogos novos")
+  if maior_ano == jogos_por_ano[ano]:
+    print(f"O ano com mais jogos novos é {ano}")
